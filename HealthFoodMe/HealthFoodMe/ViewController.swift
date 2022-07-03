5c49deeb809f8adb43b9d979c533ea8580b6a59a
L-j-h-c <ckrgkswnsgh@naver.com>
2022-07-03 15:51:26 +0900

    first commit

---
+++ HealthFoodMe/HealthFoodMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthFoodMe
+//
+//  Created by Junho Lee on 2022/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
