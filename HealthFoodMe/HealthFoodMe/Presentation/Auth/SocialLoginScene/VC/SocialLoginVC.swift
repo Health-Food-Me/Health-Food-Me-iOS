b1d6c76e7a89371e10595f935e27b401b792a8ed
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-05 21:03:39 +0900

    [Add] SocialLogin 뷰 작업을 위한 파일 생성

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Auth/SocialLoginScene/VC/SocialLoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  SocialLoginVC.swift
+//  HealthFoodMe
+//
+//  Created by 강윤서 on 2022/07/05.
+//
+
+import UIKit
+
+class SocialLoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
