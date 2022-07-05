cc3b7ae4b9dc048b1c1e05047c2edc399b3198b7
L-j-h-c <ckrgkswnsgh@naver.com>
2022-07-05 09:17:39 +0900

    [Chore] #11 - MainDetail 뷰 폴더링

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Detail/MainDetailScene/VC/MainDetailVC.swift
@@ -0,0 +1,35 @@
+//
+//  MainDetailVC.swift
+//  HealthFoodMe
+//
+//  Created by Junho Lee on 2022/07/05.
+//
+
+import UIKit
+
+import RxSwift
+
+class MainDetailVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let disposeBag = DisposeBag()
+    var viewModel: MainDetailViewModel!
+    
+    // MARK: - UI Components
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.bindViewModels()
+    }
+}
+
+// MARK: - Methods
+
+extension MainDetailVC {
+    private func bindViewModels() {
+        let input = MainDetailViewModel.Input()
+        let output = self.viewModel.transform(from: input, disposeBag: self.disposeBag)
+    }
+}
