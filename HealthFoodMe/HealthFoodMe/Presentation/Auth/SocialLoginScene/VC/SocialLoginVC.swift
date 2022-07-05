444a59f52eb7366bab50c566b7efee0fde343a7d
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-05 21:18:22 +0900

    [Add] title 에셋 추가

--- HealthFoodMe/HealthFoodMe/Presentation/Auth/SocialLoginScene/VC/SocialLoginVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Auth/SocialLoginScene/VC/SocialLoginVC.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
+import SnapKit
+
 class SocialLoginVC: UIViewController {
 
+    lazy var titleImage = UIImageView()
+    lazy var subTitleImage = UIImageView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUI() {
+        titleImage
     }
-    */
+    
 
 }
