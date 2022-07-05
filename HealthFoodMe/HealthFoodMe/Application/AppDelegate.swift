17b271e0a641c71998ad090b98f64ce6da833748
L-j-h-c <ckrgkswnsgh@naver.com>
2022-07-05 14:17:42 +0900

    [Chore] #11 - gitignore 수정 및 config 생성, NMaps ID 등록

--- HealthFoodMe/HealthFoodMe/Application/AppDelegate.swift
+++ HealthFoodMe/HealthFoodMe/Application/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import NMapsMap
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -18,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        NMFAuthManager.shared().clientId = IDLiterals.naverMapsClientID
         return true
     }
     
