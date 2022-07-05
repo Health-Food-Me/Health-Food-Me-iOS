68e94b3f163051b0c07490db8386747cb210c66d
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-05 21:37:11 +0900

    [Chore] auth 화면을 보기 위한 설정 및 코드 주석 처리

--- HealthFoodMe/HealthFoodMe/Application/SceneDelegate.swift
+++ HealthFoodMe/HealthFoodMe/Application/SceneDelegate.swift
@@ -14,23 +14,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        if #available(iOS 15.0, *) {
-            let navigationBarAppearance = UINavigationBarAppearance()
-            navigationBarAppearance.configureWithOpaqueBackground()
-            navigationBarAppearance.backgroundColor = .white
-            navigationBarAppearance.shadowColor = .white
-            
-            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
-            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
-        }
-        
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        let rootViewController = PostDetailVC()
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = rootViewController
-        self.window = window
-        window.backgroundColor = .white
-        window.makeKeyAndVisible()
+//        if #available(iOS 15.0, *) {
+//            let navigationBarAppearance = UINavigationBarAppearance()
+//            navigationBarAppearance.configureWithOpaqueBackground()
+//            navigationBarAppearance.backgroundColor = .white
+//            navigationBarAppearance.shadowColor = .white
+//            
+//            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+//            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+//        }
+//        
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        let rootViewController = PostDetailVC()
+//        let window = UIWindow(windowScene: windowScene)
+//        window.rootViewController = rootViewController
+//        self.window = window
+//        window.backgroundColor = .white
+//        window.makeKeyAndVisible()
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
--- HealthFoodMe/HealthFoodMe/Presentation/Home/PostDetailScene/Layout/PostDetailCVLayout.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Home/PostDetailScene/Layout/PostDetailCVLayout.swift
@@ -5,36 +5,36 @@
 //  Created by Junho Lee on 2022/05/18.
 //
 
-import UIKit
-
-extension PostDetailVC {
-    internal func createLayout() -> UICollectionViewCompositionalLayout {
-        return UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
-            if sectionNumber == 0 {
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(UIScreen.main.bounds.width), heightDimension: .absolute(UIScreen.main.bounds.width))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-                let section = NSCollectionLayoutSection(group: group)
-                section.orthogonalScrollingBehavior = .groupPagingCentered
-                section.visibleItemsInvalidationHandler = { items, contentOffset, environment in
-                    let currentPage = Int(max(0, round(contentOffset.x / environment.container.contentSize.width)))
-                    self.pageControl.currentPage = currentPage
-                }
-                return section
-            } else {
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(315))
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(315))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
-                let section = NSCollectionLayoutSection(group: group)
-                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(85))
-                let header =
-                NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: PostDetailUserHeader.className, alignment: .topLeading)
-                section.boundarySupplementaryItems = [header]
-                section.orthogonalScrollingBehavior = .none
-                return section
-            }
-        }
-    }
-}
+//import UIKit
+//
+//extension PostDetailVC {
+//    internal func createLayout() -> UICollectionViewCompositionalLayout {
+//        return UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
+//            if sectionNumber == 0 {
+//                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+//                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//                let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(UIScreen.main.bounds.width), heightDimension: .absolute(UIScreen.main.bounds.width))
+//                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+//                let section = NSCollectionLayoutSection(group: group)
+//                section.orthogonalScrollingBehavior = .groupPagingCentered
+//                section.visibleItemsInvalidationHandler = { items, contentOffset, environment in
+//                    let currentPage = Int(max(0, round(contentOffset.x / environment.container.contentSize.width)))
+//                    self.pageControl.currentPage = currentPage
+//                }
+//                return section
+//            } else {
+//                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(315))
+//                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(315))
+//                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+//                let section = NSCollectionLayoutSection(group: group)
+//                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(85))
+//                let header =
+//                NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: PostDetailUserHeader.className, alignment: .topLeading)
+//                section.boundarySupplementaryItems = [header]
+//                section.orthogonalScrollingBehavior = .none
+//                return section
+//            }
+//        }
+//    }
+//}
