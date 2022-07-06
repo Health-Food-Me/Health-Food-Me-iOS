99af8a06f2d2c996e41eec31049c1c22d3749c53
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-06 17:20:33 +0900

    [Feat] kakaoLogin 함수 생성

--- HealthFoodMe/HealthFoodMe/Presentation/Auth/SocialLoginScene/VC/SocialLoginVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Auth/SocialLoginScene/VC/SocialLoginVC.swift
@@ -63,3 +63,39 @@ class SocialLoginVC: UIViewController {
         }
     }
 }
+
+extension SocialLoginVC {
+    func kakaoLogin() {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            //카카오톡 로그인. api 호출 결과를 클로저로 전달.
+            UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
+                if let _ = error { self.showKakaoLoginFailMessage() }
+                else {
+                    if let accessToken = oauthToken?.accessToken {
+                        //엑세스 토큰 받아와서 서버에게 넘겨주는 로직 작성
+                        print("TOKEN", accessToken)
+                        self.postSocialLoginData(socialToken: accessToken, socialType: "KAKAO")
+                    }
+                }
+            }
+        }
+        else { //웹으로 로그인해도 똑같이 처리되도록
+            UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+                if let _ = error { self.showKakaoLoginFailMessage() }
+                else {
+                    if let accessToken = oauthToken?.accessToken {
+                        self.postSocialLoginData(socialToken: accessToken, socialType: "KAKAO")
+                    }
+                    //성공해서 성공 VC로 이동
+                }
+            }
+        }
+    }
+    
+    func showKakaoLoginFailMessage() {
+    }
+    
+    func postSocialLoginData(socialToken: String, socialType: String) {
+
+    }
+}
