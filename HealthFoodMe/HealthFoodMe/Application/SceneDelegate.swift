03389ce6a97078d4056270e4a9ffdaa902cf269f
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-06 16:51:18 +0900

    [Add] 카카오톡을 통한 사용자 인증에 필요한 함수 추가

--- HealthFoodMe/HealthFoodMe/Application/SceneDelegate.swift
+++ HealthFoodMe/HealthFoodMe/Application/SceneDelegate.swift
@@ -6,11 +6,19 @@
 //
 
 import UIKit
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+          if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            _ = AuthController.handleOpenUrl(url: url)
+          }
+        }
+      }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
