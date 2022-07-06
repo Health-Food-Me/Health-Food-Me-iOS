d7c84cd04be7736a0f59f82aa246a70c21864b0c
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-06 17:39:13 +0900

    [Add] Apple ID 유효성 검사 코드 추가

--- HealthFoodMe/HealthFoodMe/Application/AppDelegate.swift
+++ HealthFoodMe/HealthFoodMe/Application/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+
+import AuthenticationServices
 import KakaoSDKCommon
 
 @main
@@ -21,8 +23,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         KakaoSDK.initSDK(appKey : "8483f545857597ac5a551516e471a131")
         
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: "00000.abcabcabcabc.0000(로그인에 사용한 UserIdentifier)") { (credentialState, error) in
+            switch credentialState {
+            case .authorized: // 이미 증명이 된 경우 (정상)
+                print("authorized")
+            // The Apple ID credential is valid.
+            case .revoked:    // 증명을 취소했을 때,
+                print("revoked")
+                                // 로그인뷰로 이동하기
+            case .notFound:   // 증명이 존재하지 않을 경우
+                print("notFound")
+                                // 로그인뷰로 이동하기
+                
+            default:
+                break
+            }
+        }
+        
         return true
     }
+
     
     // MARK: UISceneSession Lifecycle
     
