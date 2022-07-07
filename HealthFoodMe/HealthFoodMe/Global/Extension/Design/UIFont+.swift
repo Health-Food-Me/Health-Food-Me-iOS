a05521fe2e89c5cae5478f736bffc8b206dde24f
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-07 16:17:34 +0900

    [Chore] UIFont+ 수정

--- HealthFoodMe/HealthFoodMe/Global/Extension/Design/UIFont+.swift
+++ HealthFoodMe/HealthFoodMe/Global/Extension/Design/UIFont+.swift
@@ -7,51 +7,43 @@
 
 import UIKit
 
-//extension AppFontName {
-//    static let popLight = "Poppins-Light"
-//    static let popRegular = "Poppins-Regular"
-//    static let popMedium = "Poppins-Medium"
-//    static let popBold = "Poppins-Bold"
-//    static let popExtraBold = "Poppins-ExtraBold"
-//
-//    static let notoBold = "NotoSansSC-Bold"
-//    static let notoRegular = "NotoSansSC-Regular"
-//
-//    static let nunitoExtraBold = "NunitoSans-ExtraBold"
-//}
-//
-//extension UIFont {
-//
-//    // MARK: Pretendard Font
-//    @nonobjc class func PopBold(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.popBold, size: size)!
-//    }
-//
-//    @nonobjc class func PopLight(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.popLight, size: size)!
-//    }
-//
-//    @nonobjc class func PopRegular(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.popRegular, size: size)!
-//    }
-//
-//    @nonobjc class func PopMedium(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.popMedium, size: size)!
-//    }
-//
-//    @nonobjc class func PopExtraBold(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.popExtraBold, size: size)!
-//    }
-//
-//    @nonobjc class func NotoRegular(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.notoRegular, size: size)!
-//    }
-//
-//    @nonobjc class func NotoBold(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.notoBold, size: size)!
-//    }
-//
-//    @nonobjc class func NunitoExtraBold(size: CGFloat) -> UIFont {
-//        return UIFont(name: AppFontName.nunitoExtraBold, size: size)!
-//    }
-//}
+extension AppFontName {
+    static let pretendardBold = "Pretendard-Bold"
+    static let pretendardMedium = "Pretendard-Medium"
+    static let pretendardRegular = "Pretendard-Regular"
+    
+    static let notoBold = "NotoSansCJKkr-Bold"
+    static let notoMedium = "NotoSansCJKkr-Medium"
+    static let notoRegular = "NotoSansCJKkr-Regular"
+}
+
+extension UIFont {
+    // MARK: Pretendard Font
+    @nonobjc class func PopBold(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.notoBold, size: size)!
+    }
+
+    @nonobjc class func PretendardBold(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.pretendardBold, size: size)!
+    }
+
+    @nonobjc class func PretendardMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.pretendardMedium, size: size)!
+    }
+
+    @nonobjc class func PretendardRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.pretendardRegular, size: size)!
+    }
+
+    @nonobjc class func PopExtraBold(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.notoMedium, size: size)!
+    }
+
+    @nonobjc class func NotoRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.notoRegular, size: size)!
+    }
+
+    @nonobjc class func NotoBold(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.notoBold, size: size)!
+    }
+}
