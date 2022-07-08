aa5d736d0aaba0f58f9650cc148db1fc2a764730
Choi YoungLyn <joeuni0129@gmail.com>
2022-07-09 02:54:30 +0900

    [Design] #5-CollectionView UI

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Home/MenuTabScene/Cells/MenuCellCVC.swift
@@ -0,0 +1,67 @@
+//
+//  menuCellCVC.swift
+//  HealthFoodMe
+//
+//  Created by 최영린 on 2022/07/05.
+//
+
+import UIKit
+
+import SnapKit
+
+class MenuCellCVC: UICollectionViewCell, UICollectionViewRegisterable {
+   
+    // MARK: - Properties
+    
+    static var isFromNib = false
+    
+    // MARK: - UI Components
+    
+    private lazy var menuView = MenuView()
+    private lazy var menuDetailView = MenuDetailView()
+  
+    // MARK: - Life Cycle Part
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Methods
+
+extension MenuCellCVC {
+    private func setLayout() {
+//        contentView.addSubview(menuView)
+        contentView.addSubview(menuDetailView)
+//        menuView.snp.makeConstraints { make in
+//            make.edges.equalToSuperview()
+//        }
+        menuDetailView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func setData(menuData: MenuDataModel) {
+        menuView.menuImageView.image = UIImage(named: "Image")
+        menuView.titleLabel.text = menuData.menuName
+        if menuData.isPick == true {
+            menuView.pickImageView.image = UIImage(named: "icn_pick")
+        }
+        menuView.priceLabel.text = "\(menuData.menuPrice)원"
+        menuView.kcalLabel.text = "\(menuData.menuKcal)"
+        
+        menuDetailView.titleLabel.text = menuData.menuName
+        if menuData.isPick == true {
+            menuDetailView.pickImageView.image = UIImage(named: "icn_pick")
+        }
+        menuDetailView.cAmountLabel.text = "\(menuData.carbohydrates)g"
+        menuDetailView.pAmountLabel.text = "\(menuData.protein)g"
+        menuDetailView.fAmountLabel.text = "\(menuData.fat)g"
+        menuDetailView.kcalLabel.text = "\(menuData.menuKcal)"
+    }
+}
---
+++ HealthFoodMe/HealthFoodMe/Presentation/Home/MenuTabScene/VC/MenuTabVC.swift
@@ -0,0 +1,119 @@
+//
+//  menuVC.swift
+//  HealthFoodMe
+//
+//  Created by 최영린 on 2022/07/05.
+//
+
+import UIKit
+
+import SnapKit
+
+class MenuTabVC: UIViewController {
+    
+    // MARK: - Properties
+  
+    // MARK: - UI Components
+    
+    private lazy var headerView = HeaderView()
+    private lazy var menuView = MenuView()
+    private lazy var menuDetailView = MenuDetailView()
+    
+    private lazy var menuCV: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .vertical
+        layout.sectionInset = .zero
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return cv
+    }()
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+        setDelegate()
+        setCollectionView()
+    }
+}
+
+// MARK: - Methods
+
+extension MenuTabVC {
+    private func setDelegate() {
+            menuCV.delegate = self
+            menuCV.dataSource = self
+        }
+    
+    private func setUI() {
+        menuCV.showsVerticalScrollIndicator = false
+    }
+    
+    private func setLayout() {
+        view.addSubview(headerView)
+        headerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(68)
+        }
+        
+        view.addSubview(menuCV)
+        menuCV.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setCollectionView() {
+        MenuCellCVC.register(target: menuCV)
+    }
+}
+
+// MARK: - Network
+
+extension MenuTabVC {
+
+}
+
+extension MenuTabVC: UICollectionViewDelegate {
+    
+}
+
+extension MenuTabVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return MenuDataModel.sampleMenuData.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = menuCV.dequeueReusableCell(withReuseIdentifier: MenuCellCVC.className, for: indexPath) as? MenuCellCVC
+            else { return UICollectionViewCell() }
+        cell.setData(menuData: MenuDataModel.sampleMenuData[indexPath.row])
+        return cell
+    }
+}
+
+extension MenuTabVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        
+        let cellWidth = width * (335/375)
+        let cellHeight = cellWidth * (120/335)
+        
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 15, left: 20, bottom: 15, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+}
+
+// MARK: - Network
+
+extension MenuTabVC {
+
+}
