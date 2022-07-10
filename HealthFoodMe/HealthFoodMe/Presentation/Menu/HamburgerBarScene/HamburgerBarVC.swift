7c4a090a90517a33747afe4db656a1e71a4f4839
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-10 21:31:21 +0900

    [Design] #12 - 햄버거바 UI 완성

--- HealthFoodMe/HealthFoodMe/Presentation/Menu/HamburgerBarScene/HamburgerBarVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Menu/HamburgerBarScene/HamburgerBarVC.swift
@@ -50,6 +50,31 @@ class HamburgerBarVC: UIViewController {
         settingButton.setTitle(I18N.HamburgerBar.setting, for: .normal)
         logoutButton.setTitle(I18N.HamburgerBar.logout, for: .normal)
         
+        helloLabel.textColor = .helfmeBlack
+        nameLabel.textColor = .helfmeBlack
+        sirLabel.textColor = .helfmeBlack
+        todayHelfmeLabel.textColor = .helfmeBlack
+        scrapListButton.setTitleColor(.helfmeBlack, for: .normal)
+        myReviewButton.setTitleColor(.helfmeBlack, for: .normal)
+        reportStoreButton.setTitleColor(.helfmeBlack, for: .normal)
+        repostCorrectionButton.setTitleColor(.helfmeBlack, for: .normal)
+        settingButton.setTitleColor(.helfmeBlack, for: .normal)
+        logoutButton.setTitleColor(.helfmeBlack, for: .normal)
+        
+        firstDividingLineView.backgroundColor = .helfmeLineGray
+        secondDividingLineView.backgroundColor = .helfmeLineGray
+        thirdDividingLineView.backgroundColor = .helfmeLineGray
+        
+        helloLabel.font = UIFont.PretendardRegular(size: 18)
+        nameLabel.font = UIFont.PretendardRegular(size: 18)
+        sirLabel.font = UIFont.PretendardRegular(size: 18)
+        todayHelfmeLabel.font = UIFont.PretendardRegular(size: 18)
+        scrapListButton.titleLabel?.font = UIFont.PretendardRegular(size: 16)
+        myReviewButton.titleLabel?.font = UIFont.PretendardRegular(size: 16)
+        reportStoreButton.titleLabel?.font = UIFont.PretendardRegular(size: 16)
+        repostCorrectionButton.titleLabel?.font = UIFont.PretendardRegular(size: 16)
+        settingButton.titleLabel?.font = UIFont.PretendardRegular(size: 16)
+        logoutButton.titleLabel?.font = UIFont.PretendardRegular(size: 12)
     }
     
     private func setLayout() {
@@ -64,6 +89,79 @@ class HamburgerBarVC: UIViewController {
             make.height.equalToSuperview()
             make.top.equalTo(0)
             make.leading.equalTo(0)
+        }
+        
+        helloLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(52)
+            make.leading.equalTo(20)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(helloLabel.snp.bottom).offset(3)
+            make.leading.equalTo(20)
+        }
+        
+        sirLabel.snp.makeConstraints { make in
+            make.top.equalTo(helloLabel.snp.bottom).offset(3)
+            make.leading.equalTo(nameLabel.snp.trailing).offset(2)
+        }
+        
+        editNameButton.snp.makeConstraints { make in
+            make.top.equalTo(helloLabel.snp.bottom).offset(3)
+            make.leading.equalTo(sirLabel.snp.trailing).offset(8)
+        }
+        
+        todayHelfmeLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(3)
+            make.leading.equalTo(20)
+        }
+        
+        firstDividingLineView.snp.makeConstraints { make in
+            make.top.equalTo(todayHelfmeLabel.snp.bottom).offset(38)
+            make.width.equalTo(hamburgerBarView)
+            make.height.equalTo(1)
+        }
+        
+        scrapListButton.snp.makeConstraints { make in
+            make.top.equalTo(firstDividingLineView.snp.bottom).offset(28)
+            make.leading.equalTo(20)
+        }
+        
+        myReviewButton.snp.makeConstraints { make in
+            make.top.equalTo(scrapListButton.snp.bottom).offset(20)
+            make.leading.equalTo(20)
+        }
+        
+        secondDividingLineView.snp.makeConstraints { make in
+            make.top.equalTo(myReviewButton.snp.bottom).offset(28)
+            make.width.equalTo(hamburgerBarView)
+            make.height.equalTo(1)
+        }
+        
+        reportStoreButton.snp.makeConstraints { make in
+            make.top.equalTo(secondDividingLineView.snp.bottom).offset(20)
+            make.leading.equalTo(20)
+        }
+        
+        repostCorrectionButton.snp.makeConstraints { make in
+            make.top.equalTo(reportStoreButton.snp.bottom).offset(20)
+            make.leading.equalTo(20)
+        }
+        
+        thirdDividingLineView.snp.makeConstraints { make in
+            make.top.equalTo(repostCorrectionButton.snp.bottom).offset(20)
+            make.width.equalTo(hamburgerBarView)
+            make.height.equalTo(1)
+        }
+        
+        settingButton.snp.makeConstraints { make in
+            make.top.equalTo(thirdDividingLineView.snp.bottom).offset(28)
+            make.leading.equalTo(20)
+        }
+        
+        logoutButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(14)
+            make.leading.equalTo(20)
         }
     }
 }
