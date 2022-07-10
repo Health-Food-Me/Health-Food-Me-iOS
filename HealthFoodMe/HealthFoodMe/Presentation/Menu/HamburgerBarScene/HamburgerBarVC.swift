8c93a59bdf2350d0f4b4530bd5704a5d8f672d2f
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-10 16:32:14 +0900

    [Add] 햄버거바 구현에 필요한 파일 추가

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Menu/HamburgerBarScene/HamburgerBarVC.swift
@@ -0,0 +1,18 @@
+//
+//  HamburgerBarVC.swift
+//  HealthFoodMe
+//
+//  Created by 강윤서 on 2022/07/10.
+//
+
+import UIKit
+
+class HamburgerBarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
