8a7e7f18974d47e31bd382bfd67a67864c1c3bde
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-10 20:14:25 +0900

    [Design] #12 - 햄버거바 뷰에 관한 코드 작성 1. 배경색 지정 2. 레이아웃 지정

--- HealthFoodMe/HealthFoodMe/Presentation/Menu/HamburgerBarScene/HamburgerBarVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Menu/HamburgerBarScene/HamburgerBarVC.swift
@@ -9,14 +9,49 @@ import UIKit
 
 class HamburgerBarVC: UIViewController {
 
+    private lazy var hamburgerBarView = UIView()
+    private lazy var helloLabel = UILabel()
+    private lazy var nameLabel = UILabel()
+    private lazy var sirLabel = UILabel()
+    private lazy var todayHelfmeLabel = UILabel()
+    private lazy var editNameButton = UIButton()
+    private lazy var scrapListButton = UIButton()
+    private lazy var myReviewButton = UIButton()
+    private lazy var reportStoreButton = UIButton()
+    private lazy var repostCorrectionButton = UIButton()
+    private lazy var settingButton = UIButton()
+    private lazy var logoutButton = UIButton()
+    private lazy var firstDividingLineView = UIView()
+    private lazy var secondDividingLineView = UIView()
+    private lazy var thirdDividingLineView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUI()
+        setLayout()
     }
     
     private func setUI() {
         let blackColorWithAlpha: UIColor = UIColor(.black).withAlphaComponent(0.6)
         view.backgroundColor = blackColorWithAlpha
+        hamburgerBarView.backgroundColor = .helfmeWhite
+        
+        editNameButton.setImage(UIImage(named: "btn_edit"), for: .normal)
+    }
+    
+    private func setLayout() {
+        view.addSubviews(hamburgerBarView, helloLabel, nameLabel,
+                         sirLabel, todayHelfmeLabel, editNameButton,
+                         scrapListButton, myReviewButton, reportStoreButton,
+                         repostCorrectionButton, settingButton, logoutButton,
+                         firstDividingLineView, secondDividingLineView, thirdDividingLineView)
+        
+        hamburgerBarView.snp.makeConstraints { make in
+            make.width.equalToSuperview().multipliedBy(0.71)
+            make.height.equalToSuperview()
+            make.top.equalTo(0)
+            make.leading.equalTo(0)
+        }
     }
 }
