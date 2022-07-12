ff3d1befdf531b9019124fb23598af13301b00af
L-j-h-c <ckrgkswnsgh@naver.com>
2022-07-12 23:27:53 +0900

    [Design] #8 - TagSummaryCVC 구현

--- HealthFoodMe/HealthFoodMe/Presentation/Map/MainMapScene/Cells/TagSummaryCVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Map/MainMapScene/Cells/TagSummaryCVC.swift
@@ -1,8 +1,62 @@
 //
-//  TagSummaryCVC.swift
+//  TagSummaryCollectionView.swift
 //  HealthFoodMe
 //
 //  Created by Junho Lee on 2022/07/12.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class TagSummaryCVC: UICollectionViewCell, UICollectionViewRegisterable {
+    
+    // MARK: - Properties
+    
+    static var isFromNib: Bool = false
+    
+    // MARK: - UI Components
+    
+    private let tagLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = ""
+        lb.textColor = .mainRed
+        lb.font = .NotoRegular(size: 10)
+        return lb
+    }()
+    
+    // MARK: - View Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Methods
+
+extension TagSummaryCVC {
+    private func setUI() {
+        self.layer.cornerRadius = 8
+        self.layer.borderColor = UIColor.mainRed.cgColor
+        self.layer.borderWidth = 1
+        self.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        self.addSubviews(tagLabel)
+        
+        tagLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    func setData(tag: String) {
+        self.tagLabel.text = tag
+    }
+}
