c6f4051be6aa0dc13a3ca1f744da71619e9b1960
김영인 <0inn1220@gmail.com>
2022-07-12 18:19:00 +0900

    [Design] #28 - 검색 결과 UI 구현

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Search/SearchScene/Cells/SearchResultTVC.swift
@@ -0,0 +1,133 @@
+//
+//  SearchResultTVC.swift
+//  HealthFoodMe
+//
+//  Created by 김영인 on 2022/07/12.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SearchResultTVC: UITableViewCell, UITableViewRegisterable {
+    
+    // MARK: - Properties
+    
+    static var isFromNib: Bool = false
+    
+    // MARK: - UI Components
+    
+    private var storeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .helfmeWhite
+        view.layer.cornerRadius = 15
+        view.layer.borderColor = UIColor.helfmeLineGray.cgColor
+        view.layer.borderWidth = 1
+        return view
+    }()
+    
+    private var storeImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.layer.cornerRadius = 8
+        iv.image = ImageLiterals.MainDetail.tempSalady
+        return iv
+    }()
+    
+    private var foodLabel: UILabel = {
+        let lb = UILabel()
+        lb.textColor = .mainGreen
+        lb.text = "샐러드"
+        lb.font = .NotoMedium(size: 10)
+        return lb
+    }()
+    
+    private var storeNameLabel: UILabel = {
+        let lb = UILabel()
+        lb.textColor = .helfmeBlack
+        lb.text = "써브웨이 동대문역사문화공원역점"
+        //lb.setLineSpacing(lineSpacing: 2)
+        lb.font = .NotoBold(size: 14)
+        lb.numberOfLines = 2
+        lb.lineBreakMode = .byWordWrapping
+        return lb
+    }()
+    
+    private var starView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .mainGreen
+        return view
+    }()
+    
+    private var distanceLabel: UILabel = {
+        let lb = UILabel()
+        lb.textColor = .helfmeGray1
+        lb.text = "거리: 50m"
+        lb.font = .NotoMedium(size: 10)
+        return lb
+    }()
+    
+    private lazy var storeStackView: UIStackView = {
+        let sv = UIStackView()
+        sv.addArrangedSubviews(foodLabel)
+        sv.addArrangedSubviews(storeNameLabel)
+        sv.addArrangedSubviews(starView)
+        sv.addArrangedSubviews(distanceLabel)
+        sv.alignment = .leading
+        sv.axis = .vertical
+        sv.spacing = 2
+        return sv
+    }()
+    
+    // MARK: - View Life Cycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Methods
+
+extension SearchResultTVC {
+    func setData(data: [String]) {
+        
+    }
+    
+    private func setUI() {
+        backgroundColor = .helfmeWhite
+    }
+    
+    private func setLayout() {
+        contentView.addSubviews(storeView)
+        
+        storeView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(safeAreaLayoutGuide).inset(20)
+            $0.top.bottom.equalTo(safeAreaLayoutGuide).inset(6)
+        }
+        
+        storeView.addSubviews(storeImageView)
+        
+        storeImageView.snp.makeConstraints {
+            $0.leading.equalTo(storeView.snp.leading).inset(16)
+            $0.width.height.equalTo(91)
+            $0.top.bottom.equalTo(storeView).inset(12)
+        }
+        
+        storeView.addSubviews(storeStackView)
+        
+        storeStackView.snp.makeConstraints {
+            $0.leading.equalTo(storeImageView.snp.trailing).offset(20)
+            $0.centerY.equalTo(storeView)
+        }
+        
+        storeNameLabel.snp.makeConstraints {
+            $0.width.equalTo(100)
+        }
+    }
+}
