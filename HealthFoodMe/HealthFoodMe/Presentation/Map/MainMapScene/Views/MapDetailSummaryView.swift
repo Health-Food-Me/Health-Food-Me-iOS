84d2411d91e44186fa5fc148841ff55757a7f895
L-j-h-c <ckrgkswnsgh@naver.com>
2022-07-12 23:28:38 +0900

    [Design] #8 - MapDetailSummaryView UI

--- HealthFoodMe/HealthFoodMe/Presentation/Map/MainMapScene/Views/MapDetailSummaryView.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Map/MainMapScene/Views/MapDetailSummaryView.swift
@@ -6,3 +6,140 @@
 //
 
 import Foundation
+import UIKit
+
+import SnapKit
+
+final class MapDetailSummaryView: UIView {
+    
+    // MARK: - UI Components
+    
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.image = ImageLiterals.MainDetail.tempMuseum
+        return iv
+    }()
+    
+    private let titleTagStackView: UIStackView = {
+        let st = UIStackView()
+        st.axis = .vertical
+        st.spacing = 6
+        st.distribution = .fillProportionally
+        st.alignment = .leading
+        return st
+    }()
+    
+    private let restaurantNameLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "서브웨이 동대문역사문화공원역점"
+        lb.textColor = .helfmeBlack
+        lb.lineBreakMode = .byWordWrapping
+        lb.numberOfLines = 0
+        lb.font = .NotoBold(size: 16)
+        return lb
+    }()
+    
+    private let starRateStackView: UIStackView = {
+        let st = UIStackView()
+        st.axis = .horizontal
+        st.spacing = 2
+        st.distribution = .fillProportionally
+        st.alignment = .leading
+        return st
+    }()
+    
+    private let starStackView: UIStackView = {
+        let st = UIStackView()
+        st.axis = .horizontal
+        st.spacing = 2
+        st.distribution = .equalSpacing
+        return st
+    }()
+    
+    private let rateLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "(4.3)"
+        lb.textColor = .lightGray
+        lb.font = .NotoRegular(size: 12)
+        return lb
+    }()
+    
+    private lazy var tagCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 4
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsHorizontalScrollIndicator = false
+        cv.backgroundColor = .clear
+        cv.isScrollEnabled = false
+        return cv
+    }()
+    
+    // MARK: View Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+        setDelegate()
+        registerCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Methods
+
+extension MapDetailSummaryView {
+    func setData() {
+        
+    }
+   
+    private func setUI() {
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 13
+        self.layer.applyShadow(color: .helfmeBlack, alpha: 0.1, x: 0, y: -3, blur: 4, spread: 0)
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints { make in
+            make.width.equalTo(UIScreen.main.bounds.width)
+            make.height.equalTo(140)
+        }
+        
+        self.addSubviews(logoImageView, titleTagStackView)
+        
+        logoImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.top.bottom.equalToSuperview().inset(15)
+            make.width.height.equalTo(112)
+        }
+        
+        titleTagStackView.addArrangedSubviews(restaurantNameLabel, starRateStackView, tagCollectionView)
+        starRateStackView.addArrangedSubviews(starStackView, rateLabel)
+        setStarStackView()
+        
+        restaurantNameLabel.snp.makeConstraints { make in
+            make.width.lessThanOrEqualTo(200)
+        }
+        
+        rateLabel.snp.makeConstraints { make in
+            make.height.equalTo(13)
+        }
+        
+        tagCollectionView.snp.makeConstraints { make in
+            make.width.equalTo(200)
+            make.height.equalTo(30)
+        }
+        
+        titleTagStackView.snp.makeConstraints { make in
+            make.leading.equalTo(logoImageView.snp.trailing).offset(16)
+            make.centerY.equalTo(logoImageView)
+        }
+    }
+    
