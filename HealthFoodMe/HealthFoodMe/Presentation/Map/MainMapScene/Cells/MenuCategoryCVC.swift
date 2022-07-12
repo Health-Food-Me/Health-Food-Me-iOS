cb554172b3934529aeaca86f445a07b08cd17afc
L-j-h-c <ckrgkswnsgh@naver.com>
2022-07-12 23:27:26 +0900

    [Design] #8 - MenuCategoryCVC 구현

--- HealthFoodMe/HealthFoodMe/Presentation/Map/MainMapScene/Cells/MenuCategoryCVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Map/MainMapScene/Cells/MenuCategoryCVC.swift
@@ -5,4 +5,79 @@
 //  Created by Junho Lee on 2022/07/12.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class MenuCategoryCVC: UICollectionViewCell, UICollectionViewRegisterable {
+    
+    // MARK: - Properties
+    
+    static var isFromNib: Bool = false
+    
+    override var isSelected: Bool {
+        didSet {
+            
+        }
+    }
+    
+    // MARK: - UI Components
+    
+    private let menuImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .center
+        iv.image = ImageLiterals.Map.manifyingIcon
+        return iv
+    }()
+    
+    private let menuNameLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = ""
+        lb.textColor = .black
+        lb.font = .NotoRegular(size: 14)
+        return lb
+    }()
+    
+    // MARK: - View Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: Methods
+
+extension MenuCategoryCVC {
+    private func setUI() {
+        self.layer.applyShadow(color: .helfmeBlack, alpha: 0.2, x: 0, y: 2, blur: 4, spread: 0)
+        self.layer.cornerRadius = 16
+        self.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        self.addSubviews(menuImageView, menuNameLabel)
+        
+        menuImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(24)
+            make.leading.equalToSuperview().inset(4)
+        }
+        
+        menuNameLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(menuImageView.snp.centerY)
+            make.leading.equalTo(menuImageView.snp.trailing).offset(8)
+            make.trailing.equalToSuperview().inset(12)
+        }
+    }
+    
+    func setData(data: MainMapCategory) {
+        self.menuNameLabel.text = data.menuName
+        self.menuImageView.image = data.menuIcon
+    }
+}
