e270c5f2335bb8d07a7b56c8b667bac439edce72
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-13 17:39:41 +0900

    [Add] #43 - 블로그리뷰데이터모델 생성

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Detail/BlogReviewTabScene/ViewModel/BlogReviewDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  BlogReviewDataModel.swift
+//  HealthFoodMe
+//
+//  Created by 강윤서 on 2022/07/13.
+//
+
+import Foundation
+
+struct BlogReviewDataModel {
+    let blogReviewTitle: String
+    let blogReviewContents: String
+    let blogReviewImage: String?
+}
