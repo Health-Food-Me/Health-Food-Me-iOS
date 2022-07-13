b0f615a4ee9589b850e0df06327c1188bcf89ac6
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-14 01:53:55 +0900

    [Fix] #56 - 잘못된 값 수정

--- HealthFoodMe/HealthFoodMe/Global/Extension/Design/UIColor+.swift
+++ HealthFoodMe/HealthFoodMe/Global/Extension/Design/UIColor+.swift
@@ -54,7 +54,7 @@ extension UIColor {
     }
     
     @nonobjc class var helfmeRedSub: UIColor {
-        return UIColor(red: 255.0 / 255.0, green: 255.0 / 102.0, blue: 102.0 / 255.0, alpha: 1.0)
+        return UIColor(red: 255.0 / 255.0, green: 102.0 / 255.0, blue: 102.0 / 255.0, alpha: 1.0)
     }
     
     @nonobjc class var helfmeGreenSubLight: UIColor {
