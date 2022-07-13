db64d83296a6f2492ae17dec07d05200afd62f41
김영인 <0inn1220@gmail.com>
2022-07-14 06:20:15 +0900

    [Design] #55 - 스크랩 UI 구현

--- HealthFoodMe/HealthFoodMe/Presentation/Scrap/ScrapScene/Cells/ScrapCVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Scrap/ScrapScene/Cells/ScrapCVC.swift
@@ -16,11 +16,39 @@ class ScrapCVC: UICollectionViewCell, UICollectionViewRegisterable {
     static var isFromNib: Bool = false
   
     // MARK: - UI Components
+    
+    private let storeImageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    private let scrapButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(ImageLiterals.Scrap.bookmarkInactiveIcon, for: .normal)
+        return btn
+    }()
+    
+    private let storeNameLabel: UILabel = {
+        let lb = UILabel()
+        lb.textColor = .black
+        lb.font = .NotoMedium(size: 14)
+        lb.numberOfLines = 0
+        lb.lineBreakMode = .byCharWrapping
+        return lb
+    }()
+    
+    private let locationLabel: UILabel = {
+        let lb = UILabel()
+        lb.textColor = .helfmeGray1
+        lb.font = .NotoRegular(size: 10)
+        return lb
+    }()
   
     // MARK: - Life Cycle Part
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUI()
         setLayout()
     }
 
@@ -32,11 +60,42 @@ class ScrapCVC: UICollectionViewCell, UICollectionViewRegisterable {
 // MARK: - Methods
 
 extension ScrapCVC {
+    func setData(data: ScrapDataModel) {
+        storeImageView.image = UIImage(named: data.scrapimageUrl)
+        storeNameLabel.text = data.storeName
+        locationLabel.text = data.storeLocation
+    }
+    
     private func setUI() {
-        
+        contentView.layer.borderColor = UIColor.helfmeLineGray.cgColor
+        contentView.layer.borderWidth = 1
+        contentView.layer.cornerRadius = 8
     }
     
     private func setLayout() {
+        contentView.addSubviews(storeImageView,
+                                scrapButton,
+                                storeNameLabel,
+                                locationLabel)
         
+        storeImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.height.width.equalTo(160)
+        }
+        
+        scrapButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(11)
+            $0.trailing.equalToSuperview().inset(13)
+        }
+        
+        storeNameLabel.snp.makeConstraints {
+            $0.top.equalTo(storeImageView.snp.bottom).offset(5)
+            $0.leading.trailing.equalToSuperview().inset(10)
+        }
+        
+        locationLabel.snp.makeConstraints {
+            $0.top.equalTo(storeNameLabel.snp.bottom).offset(5)
+            $0.leading.trailing.equalToSuperview().inset(10)
+        }
     }
 }
--- HealthFoodMe/HealthFoodMe/Presentation/Scrap/ScrapScene/VC/ScrapVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Scrap/ScrapScene/VC/ScrapVC.swift
@@ -33,6 +33,8 @@ class ScrapVC: UIViewController {
     
     private let scrapTitleLabel: UILabel = {
         let lb = UILabel()
+        lb.text = I18N.Scrap.scrapTitle
+        lb.font = .NotoBold(size: 16)
         return lb
     }()
     
@@ -52,6 +54,8 @@ class ScrapVC: UIViewController {
         super.viewDidLoad()
         setUI()
         setLayout()
+        setDelegate()
+        registerCell()
     }
 }
 
@@ -59,7 +63,7 @@ class ScrapVC: UIViewController {
 
 extension ScrapVC {
     private func setUI() {
-        
+        self.navigationController?.navigationBar.isHidden = true
     }
     
     private func setLayout() {
@@ -68,21 +72,81 @@ extension ScrapVC {
                          scrapCollectionView)
         
         scrapTopView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(48)
         }
         
-        scrapTopView.addSubviews(scrapBackButton)
+        scrapTopView.addSubviews(scrapBackButton, scrapTitleLabel)
         
         scrapBackButton.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(20)
             $0.width.height.equalTo(24)
+            $0.centerY.equalTo(scrapTopView)
+        }
+        
+        scrapTitleLabel.snp.makeConstraints {
+            $0.center.equalTo(scrapTopView)
         }
         
         lineView.snp.makeConstraints {
+            $0.top.equalTo(scrapTopView.snp.bottom)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(1)
         }
+        
+        scrapCollectionView.snp.makeConstraints {
+            $0.top.equalTo(lineView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setDelegate() {
+        scrapCollectionView.delegate = self
+        scrapCollectionView.dataSource = self
+    }
+    
+    private func registerCell() {
+        ScrapCVC.register(target: scrapCollectionView)
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ScrapVC: UICollectionViewDelegate {
+    
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ScrapVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return ScrapDataModel.sampleScrapData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ScrapCVC.className, for: indexPath) as? ScrapCVC else { return UICollectionViewCell() }
+        cell.setData(data: ScrapDataModel.sampleScrapData[indexPath.row])
+        return cell
+    }
+}
+
+extension ScrapVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+
+        let cellWidth = width * (160/375)
+        let cellHeight = cellWidth * (232/160)
+
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 20, bottom: 15, right: 20)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
     }
 }
 
