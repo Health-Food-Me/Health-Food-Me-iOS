6f6657a13958f87c434dccf7331600e40c88cebc
김영인 <0inn1220@gmail.com>
2022-07-13 14:10:13 +0900

    [Add] #28 - 이미지 에셋 추가

--- HealthFoodMe/HealthFoodMe/Global/Literals/ImageLiterals.swift
+++ HealthFoodMe/HealthFoodMe/Global/Literals/ImageLiterals.swift
@@ -35,5 +35,7 @@ struct ImageLiterals {
         static let star2Icon = UIImage(named: "icn_star_02")
         static let star3Icon = UIImage(named: "icn_star_03")
         static let beforeIcon = UIImage(named: "icon_before")
+        static let xIcon = UIImage(named: "icn_x")
+        static let viewMapBtn = UIImage(named: "btn_viewmap")
     }
 }
