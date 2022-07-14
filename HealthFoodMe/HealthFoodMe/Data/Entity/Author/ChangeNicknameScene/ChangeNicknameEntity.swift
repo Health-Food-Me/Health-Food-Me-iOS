eb54f454e531100e631e127453dcf51b735f2f83
i-colours-u <i.colours.u@gmail.com>
2022-07-14 18:26:51 +0900

    [Feat] Nickname Change 부분 UseCase, Repository 추가

---
+++ HealthFoodMe/HealthFoodMe/Data/Entity/Author/ChangeNicknameScene/ChangeNicknameEntity.swift
@@ -0,0 +1,12 @@
+//
+//  ChangeNicknameEntity.swift
+//  HealthFoodMe
+//
+//  Created by 송지훈 on 2022/07/14.
+//
+
+import Foundation
+
+struct ChangeNicknameEntity {
+
+}
--- HealthFoodMe/HealthFoodMe/Domain/Usecase/Auth/NicknameChangeScene/NicknameChangeUseCase.swift
+++ HealthFoodMe/HealthFoodMe/Domain/Usecase/Auth/NicknameChangeScene/NicknameChangeUseCase.swift
@@ -8,7 +8,7 @@
 import RxSwift
 import RxRelay
 
-protocol ChangeNicknameUseCase {
+protocol NicknameChangeUseCase {
   func checkNicknameHasCharacter(nickname: String)
   func checkNicknameDuplicated(nickname: String)
 
@@ -16,20 +16,20 @@ protocol ChangeNicknameUseCase {
   var nicknameHasCharacter: PublishSubject<Bool> { get set }
 }
 
-final class DefaultChangeNicknameUseCase {
+final class DefaultNicknameChangeUseCase {
   
-  private let repository: ChangeNicknameRepository
+  private let repository: NicknameChangeRepository
   private let disposeBag = DisposeBag()
   
   var nicknameDuplicated = PublishSubject<Bool>()
   var nicknameHasCharacter = PublishSubject<Bool>()
 
-  init(repository: ChangeNicknameRepository) {
+  init(repository: NicknameChangeRepository) {
     self.repository = repository
   }
 }
 
-extension DefaultChangeNicknameUseCase: ChangeNicknameUseCase {
+extension DefaultNicknameChangeUseCase: NicknameChangeUseCase {
   func checkNicknameHasCharacter(nickname: String) {
     self.nicknameHasCharacter.onNext(hasCharacter(nickname))
   }
@@ -40,15 +40,15 @@ extension DefaultChangeNicknameUseCase: ChangeNicknameUseCase {
 
       .subscribe(onNext: { [weak self] duplicated in
       guard let self = self else { return }
-        print("USECASE 에서 중복 체크해줌")
       self.nicknameDuplicated.onNext(duplicated!)
     }).disposed(by: self.disposeBag)
     
   }
   
   private func hasCharacter(_ nickname: String) -> Bool {
-    let nickRegEx = "[가-힣A-Za-z0-9\\s]{20}"
+    let nicknameExceptSpacing = nickname.replacingOccurrences(of: " ", with: "")
+    let nickRegEx = "[가-힣A-Za-z0-9]{1,12}"
     let pred = NSPredicate(format: "SELF MATCHES %@", nickRegEx)
-    return !pred.evaluate(with: nickname)
+    return !pred.evaluate(with: nicknameExceptSpacing)
   }
 }
