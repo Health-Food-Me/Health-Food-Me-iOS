d5e78de3ed34fd927fd634b01e5647802c08bc52
L-j-h-c <ckrgkswnsgh@naver.com>
2022-07-15 02:56:34 +0900

    [Feat] #42 - requestObject 메서드 구현

--- HealthFoodMe/HealthFoodMe/Data/Network/Services/AuthService.swift
+++ HealthFoodMe/HealthFoodMe/Data/Network/Services/AuthService.swift
@@ -17,19 +17,23 @@ class AuthService: BaseService {
 
 extension AuthService {
     
+//    func requestSignIn(email: String, pw: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+//        AFManager.request(AuthRouter.requestSignIn(email: email, pw: pw)).responseData { response in
+//            switch response.result {
+//            case .success:
+//                guard let statusCode = response.response?.statusCode else { return }
+//                guard let data = response.data else { return}
+//                let networkResult = self.judgeStatus(by: statusCode, data, type: SignIn.self, decodingMode: .message)
+//                completion(networkResult)
+//
+//            case .failure(let err):
+//                print(err.localizedDescription)
+//            }
+//        }
+//    }
+    
     func requestSignIn(email: String, pw: String, completion: @escaping (NetworkResult<Any>) -> Void) {
-        AFManager.request(AuthRouter.requestSignIn(email: email, pw: pw)).responseData { response in
-            switch response.result {
-            case .success:
-                guard let statusCode = response.response?.statusCode else { return }
-                guard let data = response.data else { return}
-                let networkResult = self.judgeStatus(by: statusCode, data, type: SignIn.self, decodingMode: .message)
-                completion(networkResult)
-                
-            case .failure(let err):
-                print(err.localizedDescription)
-            }
-        }
+        requestObject(AuthRouter.requestSignIn(email: email, pw: pw), type: SignIn.self, decodingMode: .message, completion: completion)
     }
     
     func requestSignUp(email: String, name: String, pw: String, completion: @escaping (NetworkResult<Any>) -> Void) {
