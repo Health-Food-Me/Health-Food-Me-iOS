43fc89c764d7decde715999b6922460556f06c4a
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-16 05:33:40 +0900

    [Add] #87 - 빈 리뷰 페이지 이미지 추가

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Detail/ReviewTabScene/Cells/ReviewEmptyViewCVC.swift
@@ -0,0 +1,26 @@
+//
+//  ReviewEmptyViewCVC.swift
+//  HealthFoodMe
+//
+//  Created by 강윤서 on 2022/07/16.
+//
+
+import UIKit
+
+class ReviewEmptyViewCVC: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    static var isFromNib = false
+    
+    // MARK: - UI Components
+  
+    // MARK: - Life Cycle Part
+
+}
+
+// MARK: - Methods
+
+extension ReviewEmptyViewCVC {
+
+}
