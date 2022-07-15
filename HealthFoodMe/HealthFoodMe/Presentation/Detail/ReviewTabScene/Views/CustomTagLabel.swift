f8189175179448f5418077db83836f8bfcd7f354
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-15 18:03:20 +0900

    [Add] #36 - 더미데이터 추가

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Detail/ReviewTabScene/Views/CustomTagLabel.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTagLabel.swift
+//  HealthFoodMe
+//
+//  Created by 강윤서 on 2022/07/15.
+//
+
+import UIKit
+
+class CustomTagLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
