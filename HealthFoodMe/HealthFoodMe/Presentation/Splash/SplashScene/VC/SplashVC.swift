c89b9681e1bf25630c85c129d58f55df5cb65d08
Choi YoungLyn <joeuni0129@gmail.com>
2022-07-17 00:46:14 +0900

    [Design] #100-스플래시 화면 UI

---
+++ HealthFoodMe/HealthFoodMe/Presentation/Splash/SplashScene/VC/SplashVC.swift
@@ -0,0 +1,57 @@
+//
+//  SplashVC.swift
+//  HealthFoodMe
+//
+//  Created by 최영린 on 2022/07/17.
+//
+
+import UIKit
+
+import Lottie
+
+class SplashVC: UIViewController {
+  
+    // MARK: - UI Components
+    
+    private let animationView: AnimationView = .init(name: "splash_ios")
+  
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+        playAnimation()
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+            let vc = ModuleFactory.resolve().makeMainMapVC()
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }
+    }
+}
+
+// MARK: - Methods
+
+extension SplashVC {
+    private func setUI() {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    private func setLayout() {
+        self.view.addSubview(animationView)
+
+        animationView.frame = self.view.bounds
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFit
+    }
+    
+    private func playAnimation() {
+        animationView.play()
+    }
+}
+
+// MARK: - Network
+
+extension SplashVC {
+
+}
