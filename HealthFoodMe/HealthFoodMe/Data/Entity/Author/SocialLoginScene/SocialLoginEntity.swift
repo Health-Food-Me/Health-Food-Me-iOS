c04534d85e7a4a344c103c8f21d3867dc3ac1a81
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-18 17:23:28 +0900

    [Add] reponseBody 구조체작성

--- HealthFoodMe/HealthFoodMe/Data/Entity/Author/SocialLoginScene/SocialLoginEntity.swift
+++ HealthFoodMe/HealthFoodMe/Data/Entity/Author/SocialLoginScene/SocialLoginEntity.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct SocialLoginEntity: Codable {
+    let user: User
+    let accessToken, refreshToken: String
+}
+
+struct User: Codable {
+    let _id: String
+    let name: String
+    let social: String
+    let socialId: String
+    let email: String
+    let scrapRestaurants: [String]
+    let refreshToken: String
+    let __v: Int
+}
