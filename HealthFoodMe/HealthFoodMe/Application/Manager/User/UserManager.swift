31cfde47dd0debc2f9c35ab6925979a89d81cae4
L-j-h-c <ckrgkswnsgh@naver.com>
2022-07-19 03:22:20 +0900

    [Feat] #128 - UserManager 수정

--- HealthFoodMe/HealthFoodMe/Application/Manager/User/UserManager.swift
+++ HealthFoodMe/HealthFoodMe/Application/Manager/User/UserManager.swift
@@ -5,7 +5,6 @@
 //  Created by Junho Lee on 2022/07/18.
 //
 
-import RxSwift
 import UIKit
 
 final class UserManager {
@@ -14,16 +13,24 @@ final class UserManager {
     
     static let shared = UserManager()
     private(set) var currentUser: User?
+    private(set) var currentLoginStatus: Bool?
     
+    @UserDefaultWrapper<String>(key: "socialToken") private(set) var socialToken
     @UserDefaultWrapper<String>(key: "accessToken") private(set) var accessToken
     @UserDefaultWrapper<String>(key: "refreshToken") private(set) var refreshToken
+    @UserDefaultWrapper<String>(key: "userIdentifier") private(set) var userIdentifier
+    @UserDefaultWrapper<Bool>(key: "socialType") private(set) var isAppleLogin
     
     var hasAccessToken: Bool { return self.accessToken != nil }
     var hasRefreshToken: Bool { return self.refreshToken != nil }
-    
-    private init() {
-        
+    var isAppleLoginned: Bool {
+        return isAppleLogin!
     }
+    var isLogin: Bool { return self.currentLoginStatus ?? false }
+    var getSocialToken: String { return self.socialToken ?? "" }
+    var getAccessToken: String { return self.accessToken ?? "" }
+    
+    private init() {}
     
     func updateAuthToken(_ accessToken: String, _ refreshToken: String) {
         self.accessToken = accessToken
@@ -34,6 +41,22 @@ final class UserManager {
         self.currentUser = currentUser
     }
     
+    func setLoginStatus(isLoginned: Bool) {
+        self.currentLoginStatus = isLoginned
+    }
+    
+    func setSocialType(isAppleLogin: Bool) {
+        self.isAppleLogin = isAppleLogin
+    }
+    
+    func setUserId(userId: String) {
+        self.userIdentifier = userId
+    }
+    
+    func setSocialToken(token: String) {
+        self.socialToken = token
+    }
+    
     func clearUserInform() {
         self.accessToken = nil
         self.refreshToken = nil
@@ -41,11 +64,9 @@ final class UserManager {
     }
     
     func reissuanceAccessToken() {
-        guard let accessToken = self.accessToken   else { return }
+        guard let accessToken = self.accessToken else { return }
         guard let refreshToken = self.refreshToken else { return }
+        
+        
     }
-}
-
-struct User {
-    
 }
