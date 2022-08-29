86516ba15034ca156459eb48dbb7705b0e54e4e8
i-colours-u <i.colours.u@gmail.com>
2022-08-30 00:13:42 +0900

    [feat] dayNumberofWeek 내에서 Swift의 weekDay와 서버에서 사용하는 number 연동하는 메서드 추가

--- HealthFoodMe/HealthFoodMe/Utils/dayNumberOfWeek.swift
+++ HealthFoodMe/HealthFoodMe/Utils/dayNumberOfWeek.swift
@@ -8,7 +8,18 @@
 import Foundation
 
 extension Date {
-    func dayNumberOfWeek() -> Int? {
-        return Calendar.current.dateComponents([.weekday], from: self).weekday
+    // Swift 상에서는 일 -> 1, 월 -> 2로 표기되지만,
+    // 현재 서버에서는 월 -> 0, 화 -> 1로 표기되고 있어서 변환하는 계산식을 추가하였습니다.
+    func dayNumberOfWeek() -> Int {
+        if let day = Calendar.current.dateComponents([.weekday], from: self).weekday {
+            switch(day) {
+                case 1      : return 6
+                case 2      : return 0
+                case 3...7  : return day - 2
+                default     : return -1
+            }
+        } else {
+            return -1
+        }
     }
 }
