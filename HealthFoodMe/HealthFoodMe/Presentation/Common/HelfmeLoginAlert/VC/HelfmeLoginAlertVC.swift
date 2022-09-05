162136b8d2c4eb083ed1e8b46f045920933b73cd
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-09-06 02:29:58 +0900

    [Design] #240 - loginAlert UI 구현

--- HealthFoodMe/HealthFoodMe/Presentation/Common/HelfmeLoginAlert/VC/HelfmeLoginAlertVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Common/HelfmeLoginAlert/VC/HelfmeLoginAlertVC.swift
@@ -8,22 +8,89 @@
 import UIKit
 
 class HelfmeLoginAlertVC: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    let width = UIScreen.main.bounds.width
+    
+    //MARK: - UI Components
+    
+    private let loginAlertView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 15
+        view.backgroundColor = .helfmeWhite
+        return view
+    }()
+    
+    private let alertMessage: UILabel = {
+        let lb = UILabel()
+        lb.text = I18N.Alert.alertLoginMessage
+        lb.font = .NotoMedium(size: 16)
+        lb.numberOfLines = 2
+        lb.textAlignment = .center
+        return lb
+    }()
+    
+    private lazy var kakaoLoginButton: UIButton = {
+       let button = UIButton()
+        button.setBackgroundImage(ImageLiterals.Common.kakaoLoginBtn, for: .normal)
+        return button
+    }()
+    
+    private lazy var appleLoginButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(ImageLiterals.Common.appleLoginBtn, for: .normal)
+        return button
+    }()
+    
+    
+    //MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+// MARK: - Extension
+
+extension HelfmeLoginAlertVC {
+    private func setUI() {
+        view.backgroundColor = .black.withAlphaComponent(0.4)
     }
     
+    private func setLayout() {
+        view.addSubviews(loginAlertView)
+        loginAlertView.addSubviews(alertMessage, kakaoLoginButton, appleLoginButton)
+        
+        loginAlertView.snp.makeConstraints {
+            let logoutWidth = width * (288/375)
+            $0.center.equalToSuperview()
+            $0.width.equalTo(logoutWidth)
+            $0.height.equalTo(logoutWidth * (219/288))
+        }
+        
+        alertMessage.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(32)
+        }
+        
+        let loginButtonWidth = UIScreen.main.bounds.width - 127
+        
+        kakaoLoginButton.snp.makeConstraints {
+            $0.top.equalTo(alertMessage.snp.bottom).offset(24)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(loginButtonWidth * 42/248)
+        }
+        
+        appleLoginButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(24)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(loginButtonWidth * 42/248)
+        }
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
