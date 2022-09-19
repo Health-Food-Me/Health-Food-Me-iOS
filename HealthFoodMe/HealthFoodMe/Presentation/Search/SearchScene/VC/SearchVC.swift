fbc108babdd9952f9c7128b3db06e5dddd401605
김영인 <0inn1220@gmail.com>
2022-09-19 19:21:33 +0900

    [Chore] #263 - 주석처리
    결과 창에서 이슈발생하여 주석처리

--- HealthFoodMe/HealthFoodMe/Presentation/Search/SearchScene/VC/SearchVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Search/SearchScene/VC/SearchVC.swift
@@ -129,7 +129,7 @@ final class SearchVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         setRecentTextField()
-        searchTextField.becomeFirstResponder()
+        //searchTextField.becomeFirstResponder()
     }
     
     override func viewDidLoad() {
