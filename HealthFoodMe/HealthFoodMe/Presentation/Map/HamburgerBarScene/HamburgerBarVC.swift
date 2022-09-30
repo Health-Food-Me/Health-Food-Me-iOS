0e82c66b5d1c5da4647dada15777835535f83232
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-09-30 13:05:16 +0900

    [Fix] feature/#302 누락 부분 반영

--- HealthFoodMe/HealthFoodMe/Presentation/Map/HamburgerBarScene/HamburgerBarVC.swift
+++ HealthFoodMe/HealthFoodMe/Presentation/Map/HamburgerBarScene/HamburgerBarVC.swift
@@ -110,7 +110,11 @@ class HamburgerBarVC: UIViewController {
     
     private lazy var needLoginImageButton: UIButton = {
         let button = UIButton()
-        button.setImage(ImageLiterals.HamburgerBar.editNameBtn, for: .normal)
+        if self.isBrowsing {
+            button.setImage(ImageLiterals.HamburgerBar.needLoginBtn, for: .normal)
+        } else {
+            button.setImage(ImageLiterals.HamburgerBar.editNameBtn, for: .normal)
+        }
         
         return button
     }()
@@ -154,6 +158,7 @@ class HamburgerBarVC: UIViewController {
         button.setTitle(I18N.Map.HamburgerBar.setting, for: .normal)
         button.setTitleColor(.helfmeBlack, for: .normal)
         button.titleLabel?.font = UIFont.PretendardRegular(size: 16)
+        button.contentHorizontalAlignment = .leading
         
         return button
     }()
@@ -235,12 +240,18 @@ extension HamburgerBarVC {
             button.setTitle(buttonTitles[buttonIndex], for: .normal)
             button.setTitleColor(.helfmeBlack, for: .normal)
             button.titleLabel?.font = UIFont.PretendardRegular(size: 16)
+            button.contentHorizontalAlignment = .leading
             menuButtons.append(button)
             
             if buttonIndex < 2 {
                 storeButtonStackView.addArrangedSubviews(menuButtons[buttonIndex])
             } else {
                 reportButtonStackView.addArrangedSubviews(menuButtons[buttonIndex])
+            }
+            
+            menuButtons[buttonIndex].snp.makeConstraints { make in
+                make.width.equalToSuperview()
+                make.leading.equalToSuperview().inset(0)
             }
         }
     }
@@ -310,7 +321,7 @@ extension HamburgerBarVC {
         
         reportButtonStackView.snp.makeConstraints { make in
             make.top.equalTo(dividingLineViews[1].snp.bottom).offset(28)
-            make.leading.equalTo(hamburgerBarView).inset(20)
+            make.leading.trailing.equalTo(hamburgerBarView).inset(20)
         }
         
         dividingLineViews[2].snp.makeConstraints { make in
@@ -322,7 +333,7 @@ extension HamburgerBarVC {
 
         settingButton.snp.makeConstraints { make in
             make.top.equalTo(dividingLineViews[2].snp.bottom).offset(28)
-            make.leading.equalTo(hamburgerBarView).inset(20)
+            make.leading.trailing.equalTo(hamburgerBarView).inset(20)
         }
         
         logoutButton.snp.makeConstraints { make in
